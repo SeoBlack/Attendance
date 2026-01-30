89cce685930c3588ead5018195b5e9dd01f498ba
Olga Shomarova <olga.shomarova@protonmail.com>
2026-01-30 14:46:44 +0200

    initial project

---
+++ attendance-backend/src/test/java/org/example/attendancebackend/AttendanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.attendancebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
