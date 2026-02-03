18aed6fd940f6af2d59b8d8c531ab6607c4b3a00
Olga Shomarova <olga.shomarova@protonmail.com>
2026-02-03 16:08:31 +0200

    rename db tables and columns

--- attendance-backend/src/main/java/org/example/attendancebackend/entity/Course.java
+++ attendance-backend/src/main/java/org/example/attendancebackend/entity/Course.java
@@ -6,17 +6,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "COURSE")
+@Table(name = "courses")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "courseid")
+    @Column(name = "course_id")
     private Long id;
 
-    @Column(name = "coursename")
+    @Column(name = "course_name")
     private String courseName;
 
     @Column(name = "description")
